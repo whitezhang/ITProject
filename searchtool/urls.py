fdbc7d374f9c8b63264c874d000115b45bda9cf3
Huaizhi Zhang <whitezhangv5@gmail.com>
2015-02-06 21:23:29 +0000

    change logo and search fileds

--- searchtool/urls.py
+++ searchtool/urls.py
@@ -4,4 +4,5 @@ from searchtool import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^index/$', views.index, name='index'),
+        url(r'^index.html', views.index, name='index'),
 )
