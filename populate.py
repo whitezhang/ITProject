7e3dae39fd1c55ca0bc3769547e5f645fa25b441
Huaizhi Zhang <whitezhangv5@gmail.com>
2015-02-07 17:27:12 +0000

    program on models and front end

---
+++ populate.py
@@ -0,0 +1,21 @@
+__author__ = 'wyatt'
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'searchWeb.settings')
+
+import django
+django.setup()
+
+from searchtool.models import User
+
+def populate():
+    addUser("Wyatt", "123456")
+
+
+def addUser(username, password):
+    u = User.objects.get_or_create(username=username,password=password)[0]
+    return u
+
+if __name__ == '__main__':
+    print "Searchtool population"
+    populate()
--- searchtool/models.py
+++ searchtool/models.py
@@ -2,10 +2,36 @@ from django.db import models
 
 # Create your models here.
 
+# User
+class User(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.username
+
 # History
 class History(models.Model):
     query = models.CharField(max_length=512)        # same query can do recommendation
     date = models.DateField()
+    user = models.ForeignKey(User, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.query
+
+# Topic
+class Topic(models.Model):
+    topic = models.CharField(max_length=32)
+    date = models.DateField()
+    user = models.ForeignKey(User, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.topic
+
+# Query
+class Query(models.Model):
+    query = models.CharField(max_length=512)
+    topic = models.ForeignKey(Topic, blank=True, null=True)
 
     def __unicode__(self):
         return self.query
@@ -15,34 +41,9 @@ class Result(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=1024)
     url = models.CharField(max_length=512)
+    query = models.ForeignKey(Query, blank=True, null=True)
 
     def __unicode__(self):
         return self.title
 
-# Query
-class Query(models.Model):
-    query = models.CharField(max_length=512)
-    result = models.ForeignKey(Result)
 
-    def __unicode__(self):
-        return self.query
-
-# Topic
-class Topic(models.Model):
-    topic = models.CharField(max_length=32)
-    date = models.DateField()
-    query = models.ForeignKey(Query)
-
-    def __unicode__(self):
-        return self.topic
-
-# User
-class User(models.Model):
-    uid = models.IntegerField(unique=True)
-    username = models.CharField(max_length=32, unique=True)
-    password = models.CharField(max_length=32)
-    history = models.ForeignKey(History)
-    topic = models.ForeignKey(Topic)
-
-    def __unicode__(self):
-        return self.username
