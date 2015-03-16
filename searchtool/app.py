9ca4b9b6a589bf0ad0084d09050cd45ef79cd9bb
Huaizhi Zhang <whitezhangv5@gmail.com>
2015-03-16 14:59:29 +0000

    regex‘

--- searchtool/app.py
+++ searchtool/app.py
@@ -1,6 +1,12 @@
 # Web App
 import urllib2
 import json
+import re
+
+def converter(text):
+    text = text.replace('&nbsp;', ',').replace('&quot;', '"').replace('&#39;', "'")
+    pattern = re.compile('<[^>]+>')
+    return pattern.sub(' ', text)
 
 def bookJSONParser(query):
     book_list = []
@@ -22,23 +28,30 @@ def bookJSONParser(query):
 
         if 'description' in volumeInfo:
             book['description'] = volumeInfo['description'].encode('utf-8')
+        else:
+            book['description'] = "Sorry. This might be a secret. :("
 
         book['authors'] = ''
-        for info in volumeInfo['authors']:
-            book['authors'] += ', '+info.encode('utf-8')
-        book['authors'] = book['authors'][2:]
+        if 'authors' in volumeInfo:
+            for info in volumeInfo['authors']:
+                book['authors'] += ', '+info.encode('utf-8')
+            book['authors'] = book['authors'][2:]
 
-        book['publishedDate'] = volumeInfo['publishedDate']
+        if 'publishedDate' in volumeInfo:
+            book['publishedDate'] = volumeInfo['publishedDate']
+        else:
+            book['publishedDate'] = 'Unknown'
         book['image'] = volumeInfo['imageLinks']['thumbnail']
 
         book['categories'] = []
-        for info in volumeInfo['categories']:
-            book['categories'].append(info.encode('utf-8'))
+        if 'categories' in volumeInfo:
+            for info in volumeInfo['categories']:
+                book['categories'].append(info.encode('utf-8'))
         # book['categories'] = volumeInfo['categories']     # List
 
         if 'searchInfo' in item:
             searchInfo = item['searchInfo']
-            book['textSnippet'] = searchInfo['textSnippet']
+            book['textSnippet'] = converter(searchInfo['textSnippet'].encode('utf-8'))
         else:
             book['textSnippet'] = "Sorry. This might be a secret. :("
 
@@ -53,7 +66,10 @@ def bookJSONParser(query):
 
 def relatedBookCrawler(query):
     related_books = []
+    query = query.encode('utf-8').replace(' ','%20')
     url = "https://www.googleapis.com/books/v1/volumes?q="+query
+    print url
+    # url ="https://www.googleapis.com/books/v1/volumes?q=Tensile%20Testing,%202nd%20Edition"
     req = urllib2.Request(url=url)
     f = urllib2.urlopen(req)
     content = f.read()
